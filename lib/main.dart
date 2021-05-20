f4afd671dd5528e9d1bb2160b08c0e75b502c141
FlutLab.io <>
2021-05-20 19:48:25 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MiLoginApp extends StatelessWidget {
       title: 'App Master Sanchez',
       theme: ThemeData(
         brightness: Brightness.light,
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.black,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       darkTheme: ThemeData(
