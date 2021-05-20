6f3605e7ab2a5bc890f397256e64cb94a25e8e33
FlutLab.io <>
2021-05-20 19:47:48 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ class _IngresoSistemaState extends State<IngresoSistema> {
                   TextField(
                     decoration: InputDecoration(
                       filled: true,
-                      fillColor: Colors.pink[200],
+                      fillColor: Colors.yellow,
                       labelText: 'Nombre de usuario',
                     ),
                   ),
@@ -97,7 +97,7 @@ class _IngresoSistemaState extends State<IngresoSistema> {
                   TextField(
                     decoration: InputDecoration(
                       filled: true,
-                      fillColor: Colors.pink[200],
+                      fillColor: Colors.yellow,
                       labelText: 'Contraseña',
                     ),
                     obscureText: true,
