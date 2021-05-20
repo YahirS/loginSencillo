0880f8d65fd27d531d884173e98533a896c3469f
FlutLab.io <>
2021-05-20 20:11:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _IngresoSistemaState extends State<IngresoSistema> {
             Column(
               children: <Widget>[
                 Image.asset('assets/image03.jpg'),
-                SizedBox(height: 16.0),
+                SizedBox(height: 5.0),
               ],
             ),
             if (!acceso)
